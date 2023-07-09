57fde34fc237eba3314a6c7d0c58bb4f04b85f9e
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-09 18:41:22 +0700

    feat: add `Player` class

---
+++ src/objects/Player.ts
@@ -0,0 +1,71 @@
+import { Vector3 } from "@fivemjs/shared";
+import { Event } from "../event";
+
+export class Player {
+	public static readonly type = "player";
+	public readonly source: number;
+
+	private constructor(src: number | string) {
+		this.source = Number(src);
+	}
+
+    public static fromSource(src: number | string): Player {
+        return new Player(src);
+    }
+
+	private get sourceStr(): string {
+		return String(this.source);
+	}
+
+	public get src(): number {
+		return this.source;
+	}
+
+	public get ped(): number {
+		return GetPlayerPed(this.sourceStr);
+	}
+
+	public get identifiers(): string[] {
+		return getPlayerIdentifiers(this.source);
+	}
+
+	public get endPoint(): string {
+		return GetPlayerEndpoint(this.sourceStr);
+	}
+
+	public get name(): string {
+		return GetPlayerName(this.sourceStr);
+	}
+
+	public get ping(): number {
+		return GetPlayerPing(this.sourceStr);
+	}
+
+	public get pos(): Vector3 {
+		return Vector3.fromArray(GetEntityCoords(this.ped));
+	}
+
+	public get rot(): Vector3 {
+		return Vector3.fromArray(GetEntityRotation(this.ped));
+	}
+
+	public get isMuted(): boolean {
+		return MumbleIsPlayerMuted(this.source);
+	}
+
+	public set isMuted(isMuted: boolean) {
+		MumbleSetPlayerMuted(this.source, isMuted);
+	}
+
+	public isAceAllowed(object: string): boolean {
+		return IsPlayerAceAllowed(this.sourceStr, object);
+	}
+
+	public drop(reason: string): void {
+		DropPlayer(this.sourceStr, reason);
+	}
+
+	public emit(eventName: string, ...args: any[]): void {
+		Event.emitClient(eventName, this.source, ...args);
+	}
+}
