bef9ff6546d1f2ba3d4936229a4e6b027668ca7c
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-09 22:11:54 +0700

    refactor: Callback is not using `Player` object

--- src/callback.ts
+++ src/callback.ts
@@ -3,22 +3,20 @@ import { randomUUID } from "./uuid";
 import { Event } from "./event";
 import { CFXEventData } from "@fivemjs/shared";
 
-function triggerClientCallback<T>(eventName: string, src: number | string, ...args: any[]): Promise<T> {
+function triggerClientCallback<T>(eventName: string, player: Player, ...args: any[]): Promise<T> {
 	const cbId = randomUUID();
-	const cbEventName = `cslib:clcb:${cbId}`;
 	const promise = new Promise<T>((resolve, reject) => {
-		Event.onceClient(cbEventName, (player: Player, data: any) => {
+		Event.onceClient(cbId, (player: Player, data: any) => {
 			resolve(data as T);
 		});
 	});
-	Event.emitClient(eventName, src, cbId, ...args);
+    player.emit(`cslib:clcb:${eventName}`, cbId, ...args);
 	return promise;
 }
 
 function registerServerCallback(eventName: string, handler: (player: Player, ...args: any[]) => void) {
-	return Event.onClient(eventName, (player: Player, cbId: string, ...args: any[]) => {
-		const cbEventName = `cslib:svcb:${cbId}`;
-		player.emit(cbEventName, handler(player, ...args));
+	return Event.onClient(`cslib:svcb:${eventName}`, (player: Player, cbId: string, ...args: any[]) => {
+		player.emit(cbId, handler(player, ...args));
 	});
 }
 
