ba3a9af0e061a17084a0e88950d1e65412f11aa3
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-09 23:21:42 +0700

    feat: add `Player` class object on event

--- src/event.ts
+++ src/event.ts
@@ -1,10 +1,24 @@
 import { Player } from "./objects/Player";
 import { CFXEventData, Cfx } from "@fivemjs/shared";
 import { Event as EventShared } from "@fivemjs/shared";
+import { listenerType } from "@fivemjs/shared";
 
 export type clientListener = (player: Player, ...args: any[]) => void;
 
 export class Event extends EventShared {
+	protected static getObjectClass(obj: any): any {
+		const objType = obj.type;
+		if (!objType) return obj;
+
+		switch (objType) {
+			case Player.type: {
+				return Player.fromSource(obj.source);
+			}
+		}
+
+		return super.getObjectClass(obj);
+	}
+
 	public static emitClient(eventName: string, target: number | string | Player, ...args: any[]): void {
 		if (target instanceof Player) {
 			target = target.src;
@@ -34,8 +48,24 @@ export class Event extends EventShared {
 		});
 		return eventData;
 	}
+
+	public static on(eventName: string, listener: listenerType): CFXEventData {
+		const handler = (...args: any[]) => {
+			listener(...Event.getClassFromArguments(...args));
+		};
+		return super.on(eventName, handler);
+	}
+
+	public static once(eventName: string, listener: listenerType): CFXEventData {
+		const handler = (...args: any[]) => {
+			listener(...Event.getClassFromArguments(...args));
+		};
+        return super.once(eventName, handler);
+	}
 }
 
+export const on = Event.on;
+export const once = Event.once;
 export const emitClient = Event.emitClient;
 export const emitAllClients = Event.emitAllClients;
 export const onClient = Event.onClient;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export * from "@fivemjs/shared";
 export * from "./uuid";
 export * from "./objects";
-export { Event, onClient, onceClient, emitClient, emitAllClients } from "./event";
+export { Event, on, once, onClient, onceClient, emitClient, emitAllClients } from "./event";
 export { Collision, CollisionSphere } from "./collision";
 export { Resource } from "./resource";
 export { Callback } from "./callback";
