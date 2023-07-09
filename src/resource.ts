8b02065b77e58ea6ee4860140a28115e35566d0d
CENSOR_1337 <viixxivvimm@gmail.com>
2023-07-09 21:51:12 +0700

    feat: add `onClient`, `onceClient` in Resource class

--- src/resource.ts
+++ src/resource.ts
@@ -1,5 +1,6 @@
 import { Resource as sharedResource } from "@fivemjs/shared";
 import { Event } from "./event";
+import { CFXEventData } from "@fivemjs/shared";
 
 export class Resource extends sharedResource {
 	public static emitAllClients(eventName: string, ...args: any[]): void {
@@ -8,5 +9,13 @@ export class Resource extends sharedResource {
 
 	public static emitClient(eventName: string, target: number, ...args: any[]): void {
 		return Event.emitClient(this.getEventName(eventName), target, ...args);
+	}
+
+	public static onClient(eventName: string, handler: (...args: any[]) => void): CFXEventData {
+		return Event.onClient(this.getEventName(eventName), handler);
+	}
+
+	public static onceClient(eventName: string, handler: (...args: any[]) => void): CFXEventData {
+		return Event.onceClient(this.getEventName(eventName), handler);
 	}
 }
